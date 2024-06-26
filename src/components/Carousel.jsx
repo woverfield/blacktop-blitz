6999de2ad0ae3fa33cfb5b3b4fb053a46d7dc41a
Wilson Overfield <wkoverfield@gmail.com>
2024-06-26 11:06:34 -0500

    Small Carousel Styling

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,16 +19,40 @@ export default function Carousel() {
       <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide} />
       {slides.slides.map((item, idx) => {
         return (
-          <Link to={(idx + 1).toString()} >
-            <h1 className={slide === idx ? "carousel-heading bg-white rounded-md p-3 px-10 text-3xl" : "hidden"}>{idx + 1} vs. {idx + 1}</h1>
-            <button className={slide === idx ? "size-btn" : "size-btn hidden"}>
-              <img
-                src={item.src}
-                alt={item.alt}
-                key={idx}
-                className={slide === idx ? "slide" : "slide hidden"}
-              />
-            </button>
+          <Link to={(idx + 1).toString()} className="flex justify-center">
+            <div className={
+                  slide === idx
+                    ? "ps-" + (idx + 1) + " player-slide"
+                    : "ps-" + (idx + 1) + " player-slide hidden"
+                }>
+              <h1
+                className={
+                  slide === idx
+                    ? "carousel-heading bg-white rounded-md p-3 px-10 text-3xl"
+                    : "hidden"
+                }
+              >
+                {idx + 1} vs. {idx + 1}
+              </h1>
+              <button
+                className={
+                  slide === idx
+                    ? "btn-" + (idx + 1) + " size-btn"
+                    : "btn-" + (idx + 1) + " size-btn hidden"
+                }
+              >
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                  key={idx}
+                  className={
+                    slide === idx
+                      ? "slide-" + (idx + 1) + " slide"
+                      : "slide-" + (idx + 1) + " slide hidden"
+                  }
+                />
+              </button>
+            </div>
           </Link>
         );
       })}
