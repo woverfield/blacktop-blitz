223b41896a12c7fabb4272e815c6c8a7113d561f
Wilson Overfield <wkoverfield@gmail.com>
2024-06-26 10:51:12 -0500

    Added Querying
    Application can now query for players from json file, not displayed yet.

--- src/components/TeamGenerator.jsx
+++ src/components/TeamGenerator.jsx
@@ -1,40 +1,64 @@
 import React, { useState, useEffect } from "react";
 import Modal from "@mui/material/Modal";
 import RefreshIcon from "@mui/icons-material/Refresh";
+import { GrNext } from "react-icons/gr";
+import allPlayers from "../data/players.json";
 
-export default function TeamGenerator() {
-    // modal state
+export default function TeamGenerator({players, formData, handleReset}) {
+  const size = players;
+  const query = formData;
+  const possiblePlayers = [];
+
+  // modal state
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+  const handleClose = () => {
+    setOpen(false);
+    handleReset();
+  };
   // round selection state
   const [p1Ready, setp1Ready] = useState(false);
   const [p2Ready, setp2Ready] = useState(false);
 
-
   const handleChange = (player) => {
     if (player === 1) {
-        setp1Ready(true);
+      setp1Ready(true);
     } else if (player === 2) {
-        setp2Ready(true);
+      setp2Ready(true);
     }
   };
 
   useEffect(() => {
     if (p1Ready === true && p2Ready === true) {
-        document.querySelector('.next-btn').style.visibility = 'visible';
+      document.querySelector(".next-btn").style.visibility = "visible";
     }
   }, [p1Ready, p2Ready]);
+
+  const fitsQuery = (player) => {
+    if (player.overall >= query.get('min') && player.overall <= query.get('max')) {
+      if (player.type == 'curr' && query.get('curr') == 'on' || player.type == 'class' && query.get('class') == 'on' || player.type == 'allt' && query.get('allt') == 'on') {
+        return true
+      }
+    } else {
+      return false;
+    }
+  }
+
+  const getPossiblePlayers = () => {
+    console.log(query)
+    console.log("Fit Query: " + allPlayers.filter(fitsQuery));
+    console.log("All Players: " + allPlayers);
+  }
 
   return (
     <div className="flex flex-col justify-center">
       <button
-        className="submit-btn bg-black rounded-md p-5 px-10 text-xl"
+        className="submit-btn bg-black rounded-md p-5 px-10 text-xl flex items-center justify-center"
         type="submit"
         onClick={handleOpen}
       >
-        SUBMIT
+        CONTINUE
+        <GrNext />
       </button>
       <Modal
         open={open}
@@ -50,19 +74,28 @@ export default function TeamGenerator() {
               <div>
                 <h2 className="text-center">Player 1 Options:</h2>
                 <ul className="flex gap-10">
-                  <button className="player-btn" onClick={() => handleChange(1)}>
+                  <button
+                    className="player-btn"
+                    onClick={() => handleChange(1)}
+                  >
                     <li>
                       <img src="https://picsum.photos/100" alt="" />
                       <p>Player Name</p>
                     </li>
                   </button>
-                  <button className="player-btn" onClick={() => handleChange(1)}>
+                  <button
+                    className="player-btn"
+                    onClick={() => handleChange(1)}
+                  >
                     <li>
                       <img src="https://picsum.photos/100" alt="" />
                       <p>Player Name</p>
                     </li>
                   </button>
-                  <button className="player-btn" onClick={() => handleChange(1)}>
+                  <button
+                    className="player-btn"
+                    onClick={() => handleChange(1)}
+                  >
                     <li>
                       <img src="https://picsum.photos/100" alt="" />
                       <p>Player Name</p>
@@ -78,19 +111,28 @@ export default function TeamGenerator() {
               <div>
                 <h2 className="text-center">Player 2 Options:</h2>
                 <ul className="flex gap-10">
-                  <button className="player-btn" onClick={() => handleChange(2)}>
+                  <button
+                    className="player-btn"
+                    onClick={() => handleChange(2)}
+                  >
                     <li>
                       <img src="https://picsum.photos/100" alt="" />
                       <p>Player Name</p>
                     </li>
                   </button>
-                  <button className="player-btn" onClick={() => handleChange(2)}>
+                  <button
+                    className="player-btn"
+                    onClick={() => handleChange(2)}
+                  >
                     <li>
                       <img src="https://picsum.photos/100" alt="" />
                       <p>Player Name</p>
                     </li>
                   </button>
-                  <button className="player-btn" onClick={() => handleChange(2)}>
+                  <button
+                    className="player-btn"
+                    onClick={() => handleChange(2)}
+                  >
                     <li>
                       <img src="https://picsum.photos/100" alt="" />
                       <p>Player Name</p>
@@ -105,6 +147,7 @@ export default function TeamGenerator() {
             >
               NEXT
             </button>
+            {getPossiblePlayers()}
           </div>
         </div>
       </Modal>
--- src/components/TeamSelection.jsx
+++ src/components/TeamSelection.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import TeamGenerator from "./TeamGenerator";
+import { create } from "@mui/material/styles/createTransitions";
 
 export default function TeamSelection(params) {
-  let size = params;
+  const size = params;
+  const [formInfo, setFormInfo] = useState(new Map());
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const search = (form) => {
+    form.preventDefault();
+    const newFormInfo = new Map(formInfo);
+    newFormInfo.set("min", form.target[0].value);
+    newFormInfo.set("max", form.target[1].value);
+    const checkboxes = form.target.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      newFormInfo.set(checkbox.name, checkbox.checked ? "on" : "off");
+    });
+    setFormInfo(newFormInfo);
+    handleSubmit();
+  };
+
+  const handleSubmit = () => {
+    setFormSubmitted(true);
+  };
+
+  const handleReset = () => {
+    setFormSubmitted(false);
+  };
 
   return (
     <main className="container mx-auto flex justify-between mt-20 text-white">
@@ -15,29 +39,47 @@ export default function TeamSelection(params) {
 
       <div className="section query">
         <h3 className="bg-white rounded-md p-5 px-10 text-xl">QUERY</h3>
-        <ul className="mt-10">
-          <li>
-            <label>Min Overall:</label>
-            <input type="text" className="text-black" />
-          </li>
-          <li>
-            <label>Max Overall:</label>
-            <input type="text" className="text-black" />
-          </li>
-          <li>
-            <label>Current</label>
-            <input type="checkbox" className="text-black" />
-          </li>
-          <li>
-            <label>Classic</label>
-            <input type="checkbox" className="text-black" />
-          </li>
-          <li>
-            <label>All-Time</label>
-            <input type="checkbox" className="text-black" />
-          </li>
-        </ul>
-        <TeamGenerator />
+        <form onSubmit={search}>
+          <ul className="mt-10">
+            <li>
+              <label>Min Overall:</label>
+              <input name="min" type="text" className="text-black" />
+            </li>
+            <li>
+              <label>Max Overall:</label>
+              <input name="max" type="text" className="text-black" />
+            </li>
+            <li>
+              <label>Current</label>
+              <input name="curr" type="checkbox" className="text-black" />
+            </li>
+            <li>
+              <label>Classic</label>
+              <input name="class" type="checkbox" className="text-black" />
+            </li>
+            <li>
+              <label>All-Time</label>
+              <input name="allt" type="checkbox" className="text-black" />
+            </li>
+          </ul>
+          <div className="flex flex-col justify-center">
+            {formSubmitted === false && (
+              <button
+                className="form-submit submit-btn bg-black rounded-md p-5 px-10 text-xl"
+                type="submit"
+              >
+                SUBMIT
+              </button>
+            )}
+          </div>
+        </form>
+        {formSubmitted === true && (
+          <TeamGenerator
+            players={size}
+            formData={formInfo}
+            handleReset={() => handleReset()}
+          />
+        )}
       </div>
 
       <div className="section invisible">
