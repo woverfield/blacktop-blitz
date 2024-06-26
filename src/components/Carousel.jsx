42b93597c3aab243fe14c3563fb8f47b5c34101a
Wilson Overfield <wkoverfield@gmail.com>
2024-06-25 20:14:52 -0500

    Styling & Functional Carousel
    Fleshed out carousel functionality and added some styling.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,32 +1,52 @@
 import React, { useState } from "react";
 import slides from "../data/carousel.json";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export default function Carousel() {
   const [slide, setSlide] = useState(0);
 
   const nextSlide = () => {
     setSlide(slide === slides.slides.length - 1 ? 0 : slide + 1);
-  }
+  };
 
   const prevSlide = () => {
     setSlide(slide === 0 ? slides.slides.length - 1 : slide - 1);
-  }
+  };
 
   return (
     <div className="carousel container mx-auto">
-      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide}/>
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide} />
       {slides.slides.map((item, idx) => {
         return (
-          <img src={item.src} alt={item.alt} key={idx} className={slide === idx ? "slide" : "slide slide-hidden"} />
+          <Link to={(idx + 1).toString()} >
+            <h1 className={slide === idx ? "carousel-heading bg-white rounded-md p-3 px-10 text-3xl" : "hidden"}>{idx + 1} vs. {idx + 1}</h1>
+            <button className={slide === idx ? "size-btn" : "size-btn hidden"}>
+              <img
+                src={item.src}
+                alt={item.alt}
+                key={idx}
+                className={slide === idx ? "slide" : "slide hidden"}
+              />
+            </button>
+          </Link>
         );
       })}
-      <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide}/>
+      <BsArrowRightCircleFill
+        className="arrow arrow-right"
+        onClick={nextSlide}
+      />
       <span className="indicators">
         {slides.slides.map((_, idx) => {
-            return (
-                <button key={idx} onClick={null} className={slide === idx ? "indicator" : "indicator indicator-inactive"}></button>
-            )
+          return (
+            <button
+              key={idx}
+              onClick={null}
+              className={
+                slide === idx ? "indicator" : "indicator indicator-inactive"
+              }
+            ></button>
+          );
         })}
       </span>
     </div>
