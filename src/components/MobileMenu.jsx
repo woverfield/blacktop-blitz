c57510c343f45e844fa165c768c274ad133783b4
Wilson Overfield <wkoverfield@gmail.com>
2024-07-08 14:44:35 -0500

    Mobile Menu Component

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { IoMenu } from "react-icons/io5";
+
+export default function MobileMenu() {
+  return (
+    <div className="">
+      <IoMenu className="text-white size-20" />
+    </div>
+  );
+}
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Logo from "../img/home-logo.svg";
 import { Link } from "react-router-dom";
-import { IoMenu } from "react-icons/io5";
+import MobileMenu from "./MobileMenu";
 
 export default function Navigation() {
   const [isMobile, setIsMobile] = useState(false);
 
-  // Function to handle window resize
   const handleResize = () => {
     if (window.innerWidth <= 640) {
       setIsMobile(true);
@@ -15,12 +14,11 @@ export default function Navigation() {
     }
   };
 
-  // Effect to handle initial mount and resize events
   useEffect(() => {
-    handleResize(); // Set initial state based on window width
-    window.addEventListener("resize", handleResize); // Add event listener for resize
+    handleResize(); 
+    window.addEventListener("resize", handleResize); 
     return () => {
-      window.removeEventListener("resize", handleResize); // Clean up event listener
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -46,12 +44,3 @@ export default function Navigation() {
     </nav>
   );
 }
-
-// Mobile menu component
-const MobileMenu = () => {
-  return (
-    <div className="">
-      <IoMenu className="text-white size-20"/>
-    </div>
-  );
-};
